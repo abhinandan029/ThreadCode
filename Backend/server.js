1a1ef24888a242613cc894e6e2ae68b87d5504fc
Abhinandan Manakapure <abhinandanaj29@gmail.com>
2026-07-15 22:50:42 +0530

    connected database addded api to show chats and contacts

--- Backend/server.js
+++ Backend/server.js
@@ -5,19 +5,35 @@ import db from './utils/database.js';
 const app = express();
 
 // --- your existing REST route stays as-is ---
-// app.get("/threadcode/chat", (req, res) => {
-//   db.execute('SELECT * FROM contacts')
-//     .then(([rows]) => {
-//       console.log("query executed");
-//       res.json(rows);
-//     })
-//     .catch((error) => {
-//       console.log(error);
-//     });
-// });
+app.get("/api/home/contacts", (req, res) => {
+  db.execute('SELECT * FROM contacts')
+    .then(([rows]) => {
+      console.log("query executed");
+      res.json(rows);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+});
+
+app.get("/api/chats", (req, res) => {
+  db.execute('SELECT * FROM contacts')
+    .then(([rows]) => {
+      console.log("query executed");
+      res.json(rows);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+});
+
+const PORT = 3000 ; 
+app.listen(PORT, () => {
+  console.log(`REST API listening at port  : ${PORT}`)
+})
 
 const hocuspocus = new Server({
-  port: process.env.PORT || 3000,
+  port : 4000,
   onConnect: async (data) => {
     console.log(`Client connected to room: ${data.documentName}`);
   },
