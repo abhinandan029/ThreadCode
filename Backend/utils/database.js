7078e2350ebde46c154703303fb03327b1490fff
Abhinandan Manakapure <abhinandanaj29@gmail.com>
2026-07-19 16:34:17 +0530

    database deployed

--- Backend/utils/database.js
+++ Backend/utils/database.js
@@ -1,7 +1,7 @@
 import mysql from 'mysql2/promise';
 
 const DB = mysql.createPool({
-  uri: process.env.DATABASE_URL,
+  uri: process.env.MYSQL_URL,
   connectionLimit: 10,
   waitForConnections: true,
   queueLimit: 0,
